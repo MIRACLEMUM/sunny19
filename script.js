5048c8be7c3fef9d46a3aa2abfe6592160f81aa1
MIRACLEMUM <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2024-12-05 00:55:50 -0800

    ggg

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-console.log("Hello, World!");
-
-let name = "Alice"; // Variable that can change
-const age = 25;     // Variable that cannot change  
-var city = "Paris"; // Older way to declare a variable
-console.log(name, age, city);
+// this is my first 
+console.log("Hello world")
+let x ="educoys"
