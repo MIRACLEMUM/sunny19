4af0061f582f5639320b76578795b591a9867af7
MIRACLEMUM <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2024-12-06 13:46:52 -0800

    ggg

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-<bu
+let age =30;
+let email = 'sunnymiraclegmail.com';
+let name ='miracle'
+
+age =25;
+
+const phone = '08115432978'
+
+console.log(`name: ${name} \nEmail: ${email} \nAge: ${age}`)
+
+let myName = "Your Name";
+let myAge = 20; // Replace with your age
+
+if (myAge >= 18) {
+  console.log(myName + ", you are eligible to vote.");
+} else {
+  console.log(myName + ", you are not eligible to vote.");
+}
+
+let b = 5 > 3;
+let c = 5 < 2;
+let d = 5 >= 3;
+let e = 5 <=3;
+ console.log(`Number 1: ${b} \nNumber2: ${c} \nNumber3: ${d} \nNumber3 ${e}`)
+ 
+//console.log(5 !== 3); // true (not equal)
+//console.log(5 > 3);   // true
+//conconsole.log(5 === 5); // true (strict equality)
+//sole.log(5 < 3);   // false
+//console.log(5 >= 3);  // true
+//console.log(5 <= 3);  // false
+
+
+
+
+
+
+
